589f3f915ba10e1f843eeac69210809d5cd3ac27
wifidu <875147715@qq.com>
2020-05-14 10:52:23 +0800

    Singlt LinkedList

--- List/SinglyLink.php
+++ List/SinglyLink.php
@@ -2,12 +2,68 @@
 namespace Link;
 
 require 'Node.php';
-require 'Sentinel.php';
 
 use Link\SinglyNode as Node;
-use Link\SinglySentinel as Sentinel;
 
 Class SinglyLink{
-    $int
+    public int $size;
+
+    public ? Node $head;
+
+    public function __construct()
+    {
+        $this->size = 0;
+        $this->head = null;
+    }
+
+     /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+
+    public function get(int $index)
+    {
+        if ($index < 0 || $index > $this->size) return -1;
+
+        $curr = $this->head;
+
+        for ($i = 0; $i < $index; $i++) $curr = $curr->next;
+
+        return $curr->val;
+    }
+
+    public function addAtIndex(int $index, int $val)
+    {
+        if ($index > $this->size) return ;
+
+        if ($index < 0) return ;
+
+        ++ $this->size;
+
+        $pred = $this->head;
+        for ($i = 0; $i < $index; ++$i) $pred = $pred->next;
+
+        $toAdd = new Node($val);
+        $toAdd = $pred->next;
+        $pred->next = $toAdd;
+    }
+
+    public function deleteAtIndex(int $index)
+    {
+        if ($index < 0 || $index >= $this->size) return ;
+
+        $this->size --;
+
+        $pred = $this->head;
+
+        for ($i = 0; $i < $index; $i++) $pred = $pred->next;
+
+        $pred->next = $pred->next->next;
+    }
+    public function addAtHead(int $val)
+    {
+        $this->addAtIndex(0, $val);
+    }
+
+    public function addAtTail(int $val)
+    {
+        $this->addAtIndex($this->size, $val);
+    }
 }
-?>
